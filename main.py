5e88a3b618db7b28886bc05f07733736247ef0ea
danilopinho75 <danilopinho11@gmail.com>
2025-01-08 11:03:38 -0300

    Iniciando Sessão Spark

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+# %%
 #importar bibliotecas
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 
+# %%
+# Iniciar Sessão do Spark
+
+spark = (
+    SparkSession.builder \
+    .master('local') \
+    .appName('Pyspark_01') \
+    .getOrCreate()
+)
+
+# %%
